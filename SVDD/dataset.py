fixed min-max normalization based on training class

SVDD/dataset.py
@@ -7,9 +7,11 @@ from torchvision.transforms import ToTensor
 class FilteredDataset(Dataset):
     
     MNIST_MIN_MAX = [[-0.8826568126678467, 9.001545906066895], [-0.666146457195282, 20.108057022094727], [-0.7820455431938171, 11.665099143981934], [-0.764577329158783, 12.895051956176758], [-0.7253923416137695, 12.683235168457031], [-0.7698502540588379, 13.103279113769531], [-0.7784181237220764, 10.457836151123047], [-0.7129780650138855, 12.057777404785156], [-0.828040361404419, 10.581538200378418], [-0.7369959950447083, 10.697040557861328]]
+    CIFAR10_MIN_MAX = [[-28.94080924987793, 13.802960395812988], [-6.681769371032715, 9.158066749572754], [-34.92462158203125, 14.419297218322754], [-10.59916877746582, 11.093188285827637], [-11.945022583007812, 10.628044128417969], [-9.691973686218262, 8.94832706451416], [-9.174939155578613, 13.847018241882324], [-6.876684188842773, 12.28237247467041], [-15.603508949279785, 15.246490478515625], [-6.132884502410889, 8.046097755432129]]
 
-    def __init__(self, dataset:Dataset, include_class_label=None, exclude_class_label=None, size=None):
+    def __init__(self, dataset:Dataset, min_max_norm_class, include_class_label=None, exclude_class_label=None, size=None):
         assert include_class_label == None or exclude_class_label == None, 'one of include or exclude class label should be none'
+        self.min_max_norm_class = min_max_norm_class
 
         self.dataset = dataset
         self.filtered_indexes = []
@@ -29,13 +31,17 @@ class FilteredDataset(Dataset):
         x, l = self.dataset[self.filtered_indexes[item]]
         x = self.to_tensor(x)
 
-        if type(self.dataset) == torchvision.datasets.mnist.MNIST:
-            # l1 global contrastive normalization https://cedar.buffalo.edu/~srihari/CSE676/12.2%20Computer%20Vision.pdf
-            x_ = torch.mean(x)
-            x = (x - x_) / torch.mean(torch.abs(x - x_))
+        # l1 global contrastive normalization https://cedar.buffalo.edu/~srihari/CSE676/12.2%20Computer%20Vision.pdf
+        x_ = torch.mean(x)
+        x = (x - x_) / torch.mean(torch.abs(x - x_))
 
-            min = FilteredDataset.MNIST_MIN_MAX[l][0]
-            max = FilteredDataset.MNIST_MIN_MAX[l][1]
+        if type(self.dataset) == torchvision.datasets.mnist.MNIST:
+            min = FilteredDataset.MNIST_MIN_MAX[self.min_max_norm_class][0]
+            max = FilteredDataset.MNIST_MIN_MAX[self.min_max_norm_class][1]
+            x = (x - min) / (max - min)
+        elif type(self.dataset) == torchvision.datasets.cifar.CIFAR10:
+            min = FilteredDataset.CIFAR10_MIN_MAX[self.min_max_norm_class][0]
+            max = FilteredDataset.CIFAR10_MIN_MAX[self.min_max_norm_class][1]
             x = (x - min) / (max - min)
 
         return x, l

SVDD/loss.py
@@ -4,6 +4,6 @@ class Loss():
 
     def __call__(self, model, batch, c):
         x, l = batch
-        f_x = model(x)
+        f_x = model(x.cuda())
         return ((f_x - c)**2).sum(dim=1).mean()
         # return (1/f_x.shape[0]) * ((f_x - c)**2).sum() + (self._lambda / 2) * ((model.get_weights())**2).sum()

SVDD/main.py
@@ -4,7 +4,7 @@ import torch
 from sklearn.metrics import roc_auc_score
 from torch.optim import Adam
 from torch.utils.data import DataLoader
-from torchvision.datasets import MNIST
+from torchvision.datasets import MNIST, CIFAR10
 from tqdm import tqdm
 
 from SVDD.dataset import FilteredDataset
@@ -40,37 +40,48 @@ def test(dataloader, model, c, testing_class):
     with torch.no_grad():
         model.eval()
         for x, l in tqdm(dataloader):
-            f_x = model(x)
-            l = (l != testing_class) * 1
+            f_x = model(x.cuda())
+            l = ((l != testing_class) * 1).cuda()
             scores.append(((f_x - c)**2).sum(dim=1))
             labels.append(l)
 
-    scores = torch.cat(scores).detach().numpy()
-    labels = torch.cat(labels).detach().numpy()
+    scores = torch.cat(scores).detach().cpu().numpy()
+    labels = torch.cat(labels).detach().cpu().numpy()
 
     result = roc_auc_score(labels, scores)
     print(testing_class, result)
 
-    pass
-
 def compute_c(dataloader, model):
     output = []
     for x, _ in dataloader:
-        output.append(model(x))
+        output.append(model(x.cuda()))
 
     return (torch.cat(output).mean(dim=0)).detach()
 
 if __name__ == '__main__':
     batch_size = 200
     for i in range(10):
-        train_dataset = FilteredDataset(MNIST(root='', train=True, download=True), include_class_label=i, size=6000)
-        train_dataloader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
-        model = Model(model_type='mnist')
+        train_dataset = FilteredDataset(MNIST(root='.', train=True, download=True), min_max_norm_class=i, include_class_label=i, size=6000)
+        train_dataloader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True, num_workers=20)
+        model = Model(model_type='mnist').cuda()
         SVDDLoss = Loss()
-        c = compute_c(train_dataloader, model)
+        c = compute_c(train_dataloader, model).cuda()
 
         train(train_dataloader, model, SVDDLoss, c)
 
-        test_dataset = FilteredDataset(MNIST(root='', train=False, download=True), exclude_class_label=11, size=10000)
+        test_dataset = FilteredDataset(MNIST(root='.', train=False, download=True), min_max_norm_class=i, exclude_class_label=11, size=10000)
+        test_dataloader = DataLoader(test_dataset, batch_size=batch_size, shuffle=True)
+        test(test_dataloader, model, c, testing_class=i)
+
+    for i in range(10):
+        train_dataset = FilteredDataset(CIFAR10(root='.', train=True, download=True), min_max_norm_class=i, include_class_label=i, size=5000)
+        train_dataloader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True, num_workers=20)
+        model = Model(model_type='cifar').cuda()
+        SVDDLoss = Loss()
+        c = compute_c(train_dataloader, model).cuda()
+
+        train(train_dataloader, model, SVDDLoss, c)
+
+        test_dataset = FilteredDataset(CIFAR10(root='.', train=False, download=True), min_max_norm_class=i, exclude_class_label=11, size=10000)
         test_dataloader = DataLoader(test_dataset, batch_size=batch_size, shuffle=True)
         test(test_dataloader, model, c, testing_class=i)

SVDD/model.py
@@ -7,19 +7,36 @@ class Model(nn.Module):
 
     def __init__(self, model_type=None):
         super(Model, self).__init__()
-        self.pool = nn.MaxPool2d(2, 2)
-        self.conv1 = nn.Conv2d(in_channels=1, out_channels=8, kernel_size=(5, 5), padding=(2, 2), bias=False)
-        self.bn1 = nn.BatchNorm2d(num_features=8, eps=1e-4, affine=False)
-        self.conv2 = nn.Conv2d(in_channels=8, out_channels=4, kernel_size=(5, 5), padding=(2, 2), bias=False)
-        self.bn2 = nn.BatchNorm2d(num_features=4, eps=1e-4, affine=False)
-        self.flatten = nn.Flatten()
-        self.out = nn.Linear(in_features=4*7*7, out_features=32, bias=False)
+
+        self.model_type = model_type
+
+        if self.model_type == 'mnist':
+            self.pool = nn.MaxPool2d(2, 2)
+            self.conv1 = nn.Conv2d(in_channels=1, out_channels=8, kernel_size=(5, 5), padding=(2, 2), bias=False)
+            self.bn1 = nn.BatchNorm2d(num_features=8, eps=1e-4, affine=False)
+            self.conv2 = nn.Conv2d(in_channels=8, out_channels=4, kernel_size=(5, 5), padding=(2, 2), bias=False)
+            self.bn2 = nn.BatchNorm2d(num_features=4, eps=1e-4, affine=False)
+            self.flatten = nn.Flatten()
+            self.out = nn.Linear(in_features=4*7*7, out_features=32, bias=False)
+        elif self.model_type == 'cifar':
+            self.pool = nn.MaxPool2d(2, 2)
+            self.conv1 = nn.Conv2d(in_channels=3, out_channels=32, kernel_size=(5, 5), padding=(2, 2), bias=False)
+            self.bn1 = nn.BatchNorm2d(num_features=32, eps=1e-4, affine=False)
+            self.conv2 = nn.Conv2d(in_channels=32, out_channels=64, kernel_size=(5, 5), padding=(2, 2), bias=False)
+            self.bn2 = nn.BatchNorm2d(num_features=64, eps=1e-4, affine=False)
+            self.conv3 = nn.Conv2d(in_channels=64, out_channels=128, kernel_size=(5, 5), padding=(2, 2), bias=False)
+            self.bn3 = nn.BatchNorm2d(num_features=128, eps=1e-4, affine=False)
+            self.flatten = nn.Flatten()
+            self.out = nn.Linear(in_features=128 * 4 * 4, out_features=128, bias=False)
 
     def forward(self, batch):
         x = self.conv1(batch)
         x = self.pool(leaky_relu(self.bn1(x)))
         x = self.conv2(x)
         x = self.pool(leaky_relu(self.bn2(x)))
+        if self.model_type == 'cifar':
+            x = self.conv3(x)
+            x = self.pool(leaky_relu(self.bn3(x)))
         x = self.flatten(x)
         x = self.out(x)
         return x