dfe5909c0578522c9006edfc1d1ddf8c47391975
zia <zia.badar@campus.tu-berlin.de>
2022-09-30 11:49:03 +0200

    added complete grad cam working on mnist

--- GradCam/main.py
+++ GradCam/main.py
@@ -1,21 +1,20 @@
+from cmath import sqrt
+
 import torch
-from torch import nn
+from matplotlib import pyplot as plt
 from torch.nn import CrossEntropyLoss
-from torch.nn.functional import one_hot
 from torch.optim import Adam
 from torch.utils.data import DataLoader
-from torchvision.datasets import MNIST, CIFAR10
-from torchvision.models import resnet18
+from torchvision.datasets import MNIST
 from torchvision.transforms import transforms, ToTensor, Resize
 from tqdm import tqdm
+from model import CustomResNet, SimpleConvNet
 
-from GradCam.model import Model
 
-
-def train():
-    dataset = MNIST(root='../', train=True, transform=transforms.Compose([ToTensor(), Resize(size=(32, 32))]))
-    dataloader = DataLoader(dataset, batch_size=32, shuffle=True)
-    model = Model(num_classes=10).cuda()
+def train(config):
+    dataset = MNIST(root='../', train=True, transform=transforms.Compose([ToTensor(), Resize(size=(config['height'], config['width']))]))
+    dataloader = DataLoader(dataset, batch_size=config['batch_size'], shuffle=True)
+    model = config['model'](num_classes=10).cuda()
     model.train()
     loss = CrossEntropyLoss()           # BCE/BCEwithLogit is used in 2-class multiclass or n-class multilabel classification problem and CrossEntopyLoss is used in n-class multiclass
     optim = Adam(model.parameters(), lr=1e-3)
@@ -23,39 +22,71 @@ def train():
     for _ in tqdm(range(1), desc='epochs'):
         for (x, l) in dataloader:
             x = x.cuda()
-            target = l.cuda()
+            l = l.cuda()
+            target = l
             predicted = model(x)
 
             model.zero_grad()
-            l = loss(predicted, target)
-            l.backward()
+            los = loss(predicted, target)
+            los.backward()
             optim.step()
+
+        evaluate(model, config, sample_evaluate=True)
 
     return model
 
-def evaluate(model):
-    dataset = MNIST(root='../', train=False, transform=transforms.Compose([ToTensor(), Resize(size=(32, 32))]))
-    dataloader = DataLoader(dataset, batch_size=32, shuffle=True, num_workers=20)
+def evaluate(model, config, sample_evaluate=False):
+    batch_size = config['batch_size']
+    config['batch_size'] = 4
+    dataset = MNIST(root='../', train=False, transform=transforms.Compose([ToTensor(), Resize(size=(config['height'], config['width']))]))
+    dataloader = DataLoader(dataset, batch_size=config['batch_size'], drop_last=True)
     model.eval()
 
     tp = 0
     all = 0
-    with torch.no_grad():
-        for (x, l) in dataloader:
-            x = x.cuda()
-            target = l.cuda()
-            predicted = model(x)
+    resize = Resize(size=(config['height'], config['width']))
+    for (x, l) in dataloader:
+        x = x.cuda()
+        l = l.cuda()
+        target = l
+        predicted = model(x)
 
-            predicted = torch.argmax(predicted, dim=1)
+        predicted = torch.argmax(predicted, dim=1)
 
-            tp += torch.sum(target == predicted).item()
-            all += target.shape[0]
+        tp += torch.sum(target == predicted).item()
+        all += target.shape[0]
 
-    print(f'accuracy: {tp/all : .2f}')
+        if sample_evaluate:
+            heat_map = model.grad_cam(x, l).detach()
+            heat_map = resize(heat_map)
+
+            stack = (int)(sqrt(config['batch_size']).real)
+            img = x.reshape(stack, stack, config['height'], config['width']).permute(0, 2, 1, 3).reshape(stack * config['height'], stack * config['width']).cpu().numpy()
+
+            heat_map_img = heat_map.reshape(stack, stack, config['height'], config['width']).permute(0, 2, 1, 3).reshape(stack * config['height'], stack * config['width']).cpu().numpy()
+
+            _, ax = plt.subplots(2)
+            ax[0].imshow(img)
+            ax[1].imshow(heat_map_img)
+            plt.show()
+            break
+
+    if not sample_evaluate:
+        print(f'accuracy: {tp/all : .2f}')
     model.train()
+    config['batch_size'] = batch_size
 
 
 if __name__ == '__main__':
 
-    model = train()
-    evaluate(model)
+    # config = {'height': 224, 'width': 224, 'batch_size': 64, 'model': CustomResNet}
+    config = {'height': 28, 'width': 28, 'batch_size': 64, 'model': SimpleConvNet}
+
+    model = train(config)
+    torch.save(model.state_dict(), 'saved_model')
+
+    # model = CustomResNet(num_classes=10).cuda()
+    # model.load_state_dict(torch.load('saved_model'))
+
+    config['batch_size'] = 4
+    evaluate(model, config)
--- GradCam/model.py
+++ GradCam/model.py
@@ -1,13 +1,98 @@
-from torch import nn
-from torchvision.models import resnet18
+import torch
+from torch import nn, Tensor
+from torch.nn import ReLU
+from torch.nn.functional import one_hot
+from torchvision.models import resnet18, ResNet
+from torchvision.models.resnet import BasicBlock
+
+class GradCamNet:
+    def __init__(self, n_classes, **kwargs):
+        super().__init__(**kwargs)
+        self.num_classes = n_classes
+
+        # https://stackoverflow.com/questions/65011884/understanding-backward-hooks
+        # https://github.com/pytorch/pytorch/issues/598#issuecomment-275489803
+        def _backward_hook(module, input, output, _self=self):
+            _self.activation_grad = output[0]
+
+        def _forward_hook(module, input, output, _self=self):
+            _self.activation = output
+
+        self._backward_hook = _backward_hook
+        self._forward_hook = _forward_hook
 
 
-class Model(nn.Module):
+    def grad_cam(self, x, l):
+        l = one_hot(l, num_classes=self.num_classes)
+        logit = self(x)
+        self.zero_grad()
+        loss = torch.sum(logit * l)
+        self.activation_grad = None
+        loss.backward()
+
+        alpha = torch.mean(self.activation_grad, dim=(2, 3))
+        grad_cam = self.relu(torch.sum(alpha[:, :, None, None] * self.activation, dim=1))
+
+        return grad_cam
+
+class CustomResNet(GradCamNet, ResNet):     # resnet18 with only first 4 layers
+    def __init__(self, num_classes):
+        super().__init__(num_classes, block=BasicBlock, layers=[2, 2, 2, 2], num_classes=num_classes)
+        self.conv1 = nn.Conv2d(1, 64, kernel_size=7, stride=2, padding=3, bias=False)
+        self.fc = nn.Linear(64, num_classes)
+
+        self.layer1.register_forward_hook(self._forward_hook)
+        self.layer1.register_backward_hook(self._backward_hook)
+
+    def _forward_impl(self, x: Tensor) -> Tensor:
+        # See note [TorchScript super()]
+        x = self.conv1(x)
+        x = self.bn1(x)
+        x = self.relu(x)
+        x = self.maxpool(x)
+
+        x = self.layer1(x)
+        # x = self.layer2(x)
+        # x = self.layer3(x)
+        # x = self.layer4(x)
+
+        x = self.avgpool(x)
+        x = torch.flatten(x, 1)
+        x = self.fc(x)
+
+        return x
+
+class SimpleConvNet(GradCamNet, nn.Module):
 
     def __init__(self, num_classes):
-        super(Model, self).__init__()
-        self.resnet = resnet18(num_classes=num_classes)
-        self.resnet.conv1 = nn.Conv2d(1, 64, kernel_size=7, stride=2, padding=3, bias=False)
+            super().__init__(num_classes)
+            self.relu = ReLU(inplace=True)
 
-    def __call__(self, x):
-        return self.resnet(x)
+            self.conv1 = nn.Conv2d(in_channels=1, out_channels=32, kernel_size=3)
+            self.relu1 = ReLU(inplace=True)
+            self.conv2 = nn.Conv2d(in_channels=32, out_channels=64, kernel_size=3)
+            self.relu2 = ReLU()
+            self.relu2.register_forward_hook(self._forward_hook)
+            self.relu2.register_backward_hook(self._backward_hook)
+            self.max_pool = nn.MaxPool2d(kernel_size=2)
+            self.drop_out1 = nn.Dropout(0.5)
+            self.flatten = nn.Flatten()
+            self.dense1 = nn.Linear(9216, 128)
+            self.relu3 = ReLU(inplace=True)
+            self.drop_out2 = nn.Dropout(0.1)
+            self.dense2 = nn.Linear(128, 10)
+
+
+    def forward(self, x: Tensor) -> Tensor:
+        x = self.conv1(x)
+        x = self.relu1(x)
+        x = self.conv2(x)
+        x = self.relu2(x)
+        x = self.max_pool(x)
+        x = self.drop_out1(x)
+        x = self.flatten(x)
+        x = self.dense1(x)
+        x = self.relu3(x)
+        x = self.drop_out2(x)
+        x = self.dense2(x)
+        return x
