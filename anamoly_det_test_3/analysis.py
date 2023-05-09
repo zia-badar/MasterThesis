5dbb769a42e751eae8df007b18b79fed144cb3e5
zia <zia.badar@campus.tu-berlin.de>
2023-05-10 00:07:48 +0500

    connect and disconnected manifold working in both 1d->2d and 2d->3d

--- anamoly_det_test_3/analysis.py
+++ anamoly_det_test_3/analysis.py
@@ -21,14 +21,32 @@ def analyse(config):
     normal_dist = MultivariateNormal(loc=torch.zeros(config['encoding_dim']), covariance_matrix=torch.eye(config['encoding_dim']))
     train_dataset = ProjectedDataset(train=True, distribution=normal_dist, projection=Projection(config))
 
-    start, end, step = -1, 1, 0.01
+    train_dataloader = DataLoader(train_dataset, batch_size=config['batch_size'], shuffle=True, num_workers=config['num_workers'])
+    train_projection = []
+    with torch.no_grad():
+        for x, _ in train_dataloader:
+            train_projection.append(x)
+    train_projection = torch.cat(train_projection)
+
+    min = torch.min(train_projection, dim=0).values - 0.1
+    max = torch.max(train_projection, dim=0).values + 0.1
 
     if config['projection_dim'] == 3:
-        x, y, z = torch.arange(0, 1, step), torch.arange(0.3, 0.6, step), torch.arange(-1, 0.5, step)
+        if config['manifold_type'] == 'connected':
+            step = 0.03
+            x, y, z = torch.arange(min[0], max[0], step), torch.arange(min[1], max[1], step), torch.arange(min[2], max[2], step)
+        elif config['manifold_type'] == 'disconnected':
+            step = 0.02
+            x, y, z = torch.arange(min[0], max[0], step), torch.arange(min[1], max[1], step), torch.arange(min[2], max[2], step)
         grid_x, grid_y, grid_z = torch.meshgrid(x, y, z)
         grid_samples = torch.stack([grid_x, grid_y, grid_z]).reshape(3, -1).t().cuda()
     elif config['projection_dim'] == 2:
-        x, y = torch.arange(-1, 2, step), torch.arange(-2, 0, step)
+        if config['manifold_type'] == 'connected':
+            step = 0.01
+            x, y = torch.arange(min[0], max[0], step), torch.arange(min[1], max[1], step)
+        elif config['manifold_type'] == 'disconnected':
+            step = 0.01
+            x, y = torch.arange(min[0], max[0], step), torch.arange(min[1], max[1], step)
         grid_x, grid_y = torch.meshgrid(x, y)
         grid_samples = torch.stack([grid_x, grid_y]).reshape(2, -1).t().cuda()
 
@@ -100,11 +118,17 @@ def analyse(config):
         ax = fig.subplots(1, 2)
 
     if config['projection_dim'] == 3:
-        ax[0].scatter(xs=train_projection[:, 0].cpu().numpy(), ys = train_projection[:, 1].cpu().numpy(), zs = train_projection[:, 2].cpu().numpy())
-        percentage = 1
+        ax[0].scatter(xs=train_projection[:, 0].cpu().numpy(), ys = train_projection[:, 1].cpu().numpy(), zs = train_projection[:, 2].cpu().numpy(), marker='.')
+        if config['manifold_type'] == 'connected':
+            percentage = 6
+        elif config['manifold_type'] == 'disconnected':
+            percentage = 1
     elif config['projection_dim'] == 2:
-        ax[0].scatter(x=train_projection[:, 0].cpu().numpy(), y = train_projection[:, 1].cpu().numpy())
-        percentage = 8
+        ax[0].scatter(x=train_projection[:, 0].cpu().numpy(), y = train_projection[:, 1].cpu().numpy(), marker='.')
+        if config['manifold_type'] == 'connected':
+            percentage = 40
+        elif config['manifold_type'] == 'disconnected':
+            percentage = 10
 
     sorted_index = torch.argsort(prob, descending=True)
     indexes = sorted_index[:(int)(prob.shape[0] * percentage / 100)]
--- anamoly_det_test_3/models.py
+++ anamoly_det_test_3/models.py
@@ -48,38 +48,30 @@ class Projection(nn.Module):
 
     def __init__(self, config):
         super(Projection, self).__init__()
+        self.config = config
 
         self.projection_1 = nn.Sequential(
             nn.Linear(in_features=config['encoding_dim'], out_features=config['projection_dim']),
-            BatchNorm1d(num_features=config['projection_dim']),
-            nn.ReLU(),
+            nn.LeakyReLU(negative_slope=0.2, inplace=True),
             nn.Linear(in_features=config['projection_dim'], out_features=config['projection_dim']),
-            BatchNorm1d(num_features=config['projection_dim']),
-            nn.ReLU(),
-            nn.Linear(in_features=config['projection_dim'], out_features=config['projection_dim']),
-            BatchNorm1d(num_features=config['projection_dim']),
+            nn.LeakyReLU(negative_slope=0.2, inplace=True),
         )
 
         self.projection_2 =  nn.Sequential(
             nn.Linear(in_features=config['encoding_dim'], out_features=config['projection_dim']),
-            BatchNorm1d(num_features=config['projection_dim']),
-            nn.ReLU(),
+            nn.LeakyReLU(negative_slope=0.2, inplace=True),
             nn.Linear(in_features=config['projection_dim'], out_features=config['projection_dim']),
-            BatchNorm1d(num_features=config['projection_dim']),
-            nn.ReLU(),
-            nn.Linear(in_features=config['projection_dim'], out_features=config['projection_dim']),
-            BatchNorm1d(num_features=config['projection_dim']),
+            nn.LeakyReLU(negative_slope=0.2, inplace=True),
         )
 
-        with open(f'results/projections/{config["encoding_dim"]}d_{config["projection_dim"]}d/projection', 'rb') as file:
+        with open(f'results/projections/{config["manifold_type"]}/{config["encoding_dim"]}d_{config["projection_dim"]}d/projection', 'rb') as file:
             self.load_state_dict(torch.load(file))
 
         # torch.save(self.state_dict(), 'results/projection')
 
-    def disconnect_1d(self, x):
-        return torch.where(torch.unsqueeze(x[:, 0] < 0, -1), self.projection_1(x), self.projection_2(x))
-
     def forward(self, x):
         with torch.no_grad():
-
-            return self.disconnect_1d(x)
+            if self.config['manifold_type'] == 'connected':
+                return self.projection_1(x)
+            elif self.config['manifold_type'] == 'disconnected':
+                return torch.where(torch.unsqueeze(x[:, 0] < 0, -1), self.projection_1(x), self.projection_2(x))
